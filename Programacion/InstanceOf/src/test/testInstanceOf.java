8d6bd07a6f4e7c56e629771c67e5f709d4ff9501
Diego <diegohd-20@outlook.com>
2022-07-11 22:46:29 -0500

    instanceOf

---
+++ Programacion/InstanceOf/src/test/testInstanceOf.java
@@ -0,0 +1,28 @@
+package test;
+
+import domain.*;
+
+/**
+ *
+ * @author Diego
+ */
+public class testInstanceOf {
+
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Juan", 600.50);
+        determinarTipo(empleado);
+
+        empleado = new Gerente("karla", 7000.50, "Marketing");
+        determinarTipo(empleado);
+    }
+
+    public static void determinarTipo(Empleado empleado) {
+        if (empleado instanceof Gerente) {
+            System.out.println("Es de tipo Gerente ");
+        } else if (empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado");
+        } else if (empleado instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
+    }
+}
