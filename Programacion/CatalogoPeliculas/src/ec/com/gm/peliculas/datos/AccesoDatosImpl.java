61f174cb5ccf3350e22c46f09939ac3f06de298f
Diego <diegohd-20@outlook.com>
2022-07-24 20:11:13 -0500

    metodo listar peliculas

--- Programacion/CatalogoPeliculas/src/ec/com/gm/peliculas/datos/AccesoDatosImpl.java
+++ Programacion/CatalogoPeliculas/src/ec/com/gm/peliculas/datos/AccesoDatosImpl.java
@@ -1,10 +1,60 @@
-//package ec.com.gm.peliculas.datos;
-//
-//import ec.com.gm.peliculas.domain.Pelicula;
-//import ec.com.gm.peliculas.excepciones.AccesoDatosEx;
-//import java.io.File;
-//
-//public class AccesoDatosImpl implements IAccesoDatos {
-//
-//
-//}
+package ec.com.gm.peliculas.datos;
+
+import ec.com.gm.peliculas.domain.Pelicula;
+import ec.com.gm.peliculas.excepciones.*;
+import java.io.*;
+import java.util.*;
+
+public class AccesoDatosImpl implements IAccesoDatos {
+
+    @Override
+    public boolean existe(String nombreArchivo) throws AccesoDatosEx {
+        File archivo = new File(nombreArchivo);
+        return archivo.exists();
+    }
+
+    @Override
+    public List<Pelicula> listar(String nombreArchivo) throws LecturaDatosEx {
+        var archivo = new File(nombreArchivo);
+        List<Pelicula> peliculas = new ArrayList<>();
+        try {
+            BufferedReader entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            while (linea != null) {
+                var pelicula = new Pelicula(linea);
+                peliculas.add(pelicula);
+                linea = entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas: " + ex.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas: " + ex.getMessage());
+        }
+        return peliculas;
+    }
+
+    @Override
+    public void Escribir(Pelicula pelicula, String nobreArchivo, boolean anexar) throws EscrituraDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String buscar(String nombreArchivo, String buscar) throws LecturaDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void crear(String nombreArchivo) throws AccesoDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void borrar(String nombreArchivo) throws AccesoDatosEx {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
